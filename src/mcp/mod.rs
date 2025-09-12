ddea787114f12d2f3dc72901bca3bb54a1ac11f5
Ezra Lim <ezralim1992@gmail.com>
2025-09-12 09:13:55 +0800

    feat: Use rmcp crate

--- src/mcp/mod.rs
+++ src/mcp/mod.rs
@@ -1,2 +1,33 @@
-pub mod commands;
-pub mod transport;
+use crate::config::McpConfig;
+use rmcp::{
+    model::CallToolRequestParam,
+    service::ServiceExt,
+    transport::{ConfigureCommandExt, TokioChildProcess},
+    RmcpError,
+};
+use tokio::process::Command;
+
+#[allow(dead_code)]
+pub async fn init(_config: McpConfig) -> Result<(), RmcpError> {
+    let client = ()
+        .serve(
+            TokioChildProcess::new(Command::new("uvx").configure(|cmd| {
+                cmd.arg("mcp-server-git");
+            }))
+            .map_err(RmcpError::transport_creation::<TokioChildProcess>)?,
+        )
+        .await?;
+    let server_info = client.peer_info();
+    log::info!("Connected to server: {:?}", server_info);
+    let tools = client.list_tools(Default::default()).await?;
+    log::info!("Available tools: {:?}", tools);
+    let tool_result = client
+        .call_tool(CallToolRequestParam {
+            name: "git_status".into(),
+            arguments: serde_json::json!({ "repo_path": "." }).as_object().cloned(),
+        })
+        .await?;
+    log::info!("Tool result: {:?}", tool_result);
+    client.cancel().await?;
+    Ok(())
+}
