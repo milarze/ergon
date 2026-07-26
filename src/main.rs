b25b93929d66709550385835579d325ebaca3911
Ezra Lim <ezralim1992@gmail.com>
2026-07-26 12:53:03 +0800

    feat: add default and minimum sizes for the application window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use ergon::Ergon;
+use iced::Size;
 
 pub fn main() -> iced::Result {
     simple_logger::SimpleLogger::new()
@@ -9,6 +10,12 @@ pub fn main() -> iced::Result {
         .subscription(ergon::subscription)
         .theme(theme)
         .font(iced_fonts::LUCIDE_FONT_BYTES)
+        .window(iced::window::Settings {
+            size: Size::new(1600.0, 1200.0),
+            min_size: Some(Size::new(400.0, 300.0)),
+            resizable: true,
+            ..Default::default()
+        })
         .run()
 }
 
